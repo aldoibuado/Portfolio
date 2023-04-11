386a8897ec6bbdfdfe7f3c7465d8df0f3621bce6
aldoibuado <aldo.ibuado@gmail.com>
2023-04-11 11:14:28 -0700

    styling button and added styles file for home.css to change the size of profile pic

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,36 +1,45 @@
 import React from "react";
 import hero from "../assests/heroImage.jpg";
 import { RiArrowRightLine } from "react-icons/ri";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <div
       name="home"
       className="h-screen w-full bg-gradient-to-b
-    from-black to-gray-800"
+    from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center
-      justify-center h-full px-4 md:flex-row">
-        <div>
-          <h2>I am a Full Stack Web Developer</h2>
-          <p>
+      <div
+        className="max-w-screen-lg mx-auto flex flex-col items-center
+      justify-center h-full px-4 md:flex-row"
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I am a Full Stack Web Developer
+          </h2>
+          <p className="text-gray-500 p-4 max-w-md">
             My name is Aldo Ibuado and I have graduated from my full-stack web
             developer program. I am excited to continue to put my skills to good
             use!
           </p>
 
           <div>
-            <button>
+            <button
+              className=" group text-white w-fit px-6 py-3 my-2
+            flex items-center rounded-md bg-gradient-to-r from-cyan-500
+            to-blue-500 cursor-pointer"
+            >
               Portfolio
-              <span>
-                <RiArrowRightLine />
+              <span className="group-hover:rotate-90 duration-300">
+                <RiArrowRightLine size={20} className="ml-1" />
               </span>
             </button>
           </div>
         </div>
 
         <div>
-          <img src={hero} alt="my profile pic" />
+          <img src={hero} alt="my profile pic" className="profileimg" />
         </div>
       </div>
     </div>
