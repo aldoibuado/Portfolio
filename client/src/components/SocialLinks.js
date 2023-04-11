898cf2051c03e38eede4f15bda4e38519d26bb6f
aldoibuado <aldo.ibuado@gmail.com>
2023-04-11 15:38:26 -0700

    working on mapping social links and making sure they are clickable

--- client/src/components/SocialLinks.js
+++ client/src/components/SocialLinks.js
@@ -1,22 +1,75 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 function SocialLinks() {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          Linkedin
+          <FaLinkedin size={25} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/aldo-ibuado",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub
+          <FaGithub size={25} />
+        </>
+      ),
+      href: "https://github.com/aldoibuado",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          E-mail
+          <HiOutlineMail size={25} />
+        </>
+      ),
+      href: "mailto:aldo.ibuado@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume
+          <BsFillPersonLinesFill size={25} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 
-        px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
-          <a href=""
-             className="flex justify-between items-center w-full text-white"
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className="flex justify-between items-center w-40 h-14 
+         px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500"
           >
-            <>
-              Linkedin
-              <FaLinkedin size={25}/>
-            </>
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
