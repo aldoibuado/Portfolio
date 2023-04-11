d164e90b2117ee3e85c1ff23c3b7a42f53ac8328
aldoibuado <aldo.ibuado@gmail.com>
2023-04-11 10:16:37 -0700

    working on styling home component and adding profile picture

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import hero from '../assests/heroImage.jpg';
-import { BsArrowRightSquareFill } from "react-icons/bs";
-
+import React from "react";
+import hero from "../assests/heroImage.jpg";
+import { RiArrowRightLine } from "react-icons/ri";
 
 function Home() {
   return (
-    <div>
-        
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b
+    from-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center
+      justify-center h-full px-4 md:flex-row">
+        <div>
+          <h2>I am a Full Stack Web Developer</h2>
+          <p>
+            My name is Aldo Ibuado and I have graduated from my full-stack web
+            developer program. I am excited to continue to put my skills to good
+            use!
+          </p>
+
+          <div>
+            <button>
+              Portfolio
+              <span>
+                <RiArrowRightLine />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img src={hero} alt="my profile pic" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
