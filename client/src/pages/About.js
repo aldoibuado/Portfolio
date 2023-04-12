77491cd7ebd0ac233660c053e3ca4b2f5fe38587
aldoibuado <aldo.ibuado@gmail.com>
2023-04-11 18:09:36 -0700

    uninstalled an npm package, finished styling about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { LoremIpsum } from "react-lorem-ipsum";
 
 function About() {
   return (
@@ -8,16 +7,21 @@ function About() {
       className="w-full h-screen bg-gradient-to-b 
     from-gray-800 to-black text-white"
     >
-      <div>
-        <div>
-          <p>About</p>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col
+      justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4
+          border-gray-500">About</p>
         </div>
 
-        <LoremIpsum p={1} />
+       <p className="text-xl mt-20">Being able to brainstrom, work with a team, be creative and come up with new web applications is what 
+        got me interested in becoming a web developer!</p>
 
         <br />
-
-        <LoremIpsum p={1} />
+        
+       <p className="text-xl">Lorem ipsum dolor sit amet. Ea dolores amet qui vero excepturi eos dicta perferendis ut perferendis delectus quo consectetur labore. 
+        Vel voluptas incidunt nam odit deleniti non voluptatem tempora ut itaque distinctio. Sed temporibus eligendi et dolores nemo et error illum qui dolorem unde.</p>
+        
       </div>
     </div>
   );
