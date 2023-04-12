b25e7907b52216eb99f4bb9319505a9034035795
aldoibuado <aldo.ibuado@gmail.com>
2023-04-12 13:49:58 -0700

    working on structuring the experience page

--- client/src/pages/Experience.js
+++ client/src/pages/Experience.js
@@ -4,9 +4,42 @@ import github from "../assests/github.png";
 import graphgl from "../assests/graphql.png";
 import javascript from "../assests/javascript.png";
 import react from "../assests/react.png";
-import "../styles/Experience.css";
+// import "../styles/Experience.css";
 
 function Experience() {
+  const skills = [
+    {
+      id: 1,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 2,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-500",
+    },
+    {
+      id: 3,
+      src: graphgl,
+      title: "GraphQL",
+      style: "shadow-pink-400",
+    },
+    {
+      id: 4,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 5,
+      src: react,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+  ];
+
   return (
     <div
       name="experience"
@@ -31,11 +64,16 @@ function Experience() {
           className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8
             text-center py-8 px-12 sm:px-0"
         >
-          <div className="shadow-md hover:scale-105 duration-500
-          py-2 rounded-lg">
-            <img src={css} alt="" className="experience" />
-            <p>CSS</p>
-          </div>
+          {skills.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500
+              py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
